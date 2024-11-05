41e6c8ac3451b8da872cda72db6345c834287c98
emreasci <emreaasci@gmail.com>
2024-11-05 13:17:14 +0300

    Initial Commit

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Emre Aşcı on 5.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
