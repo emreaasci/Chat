5a2402a5a8b997a527a07441f8951818bed00ea4
emreasci <emreaasci@gmail.com>
2024-11-06 10:16:09 +0300

    .

---
+++ Chat/Screens/Channels/Views/MessageListView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageListView.swift
+//  Chat
+//
+//  Created by Emre Aşcı on 6.11.2024.
+//
+
+import SwiftUI
+
+struct MessageListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageListView()
+}
