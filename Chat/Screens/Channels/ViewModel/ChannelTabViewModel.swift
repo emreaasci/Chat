3be156ac970da9cff10a709b3c5aa7910ff00d13
emreasci <emreaasci@gmail.com>
2024-11-19 16:22:27 +0300

    .

---
+++ Chat/Screens/Channels/ViewModel/ChannelTabViewModel.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+final class ChannelTabViewModel: ObservableObject {
+    
+    @Published var navigateToChatRoom = false
+    @Published var newChannel: ChannelItem?
+    @Published var showChatPartnerPickerView = false
+    
+    func onNewChannelCreation(_ channel: ChannelItem) {
+        showChatPartnerPickerView = false
+        newChannel = channel
+        navigateToChatRoom = true
+    }
+}
---
+++ Chat/Screens/Chat/Models/MessageItem+Types.swift
@@ -0,0 +1,8 @@
+//
+//  MessageItem+Types.swift
+//  Chat
+//
+//  Created by Emre Aşcı on 19.11.2024.
+//
+
+import Foundation
